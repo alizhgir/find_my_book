ff8e06749eff2a05497696d0b8e9539c4940cfc7
ruslan-DS <ruslanvolo9@gmail.com>
2023-12-15 13:17:11 +0300

    Correct code and update users experience.

--- model/bert.py
+++ model/bert.py
@@ -6,13 +6,21 @@ import faiss
 from transformers import AutoTokenizer, AutoModel
 
 
-weight = "cointegrated/rubert-tiny2"
+CHECKPOINT = "cointegrated/rubert-tiny2"
 
-tokenizer = AutoTokenizer.from_pretrained(weight)
-model = AutoModel.from_pretrained(weight)
+tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+model = AutoModel.from_pretrained(CHECKPOINT)
 
 vectors_annotation = np.load('datasets/annotation_embeddings2.npy')
-data_frame = pd.read_csv('datasets/cleaned_final_books.csv')
+
+data_frame = pd.read_csv('datasets/final_dataset.csv')
+
+data_frame = pd.DataFrame({
+    'Cсылка на книгу': data_frame['page_url'],
+    'Обложка': data_frame['image_url'],
+    'Инфо': data_frame[['category_name', 'age', 'title', 'author']].agg(', '.join, axis=1),
+    'Аннотация': data_frame['annotation']
+})
 
 MAX_LEN = 512
 
@@ -21,24 +29,22 @@ faiss_index = faiss.IndexFlatL2(312)
 faiss_index.add(vectors_annotation)
 
 
-def recommend(text, top_k):
+def recommend(query: str, top_k: int) -> pd.DataFrame:
 
-    tokenized_text = tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=MAX_LEN)
+    tokenized_text = tokenizer.encode(query, add_special_tokens=True, truncation=True, max_length=MAX_LEN)
     tokenized_text = torch.tensor(tokenized_text).unsqueeze(0)
 
     with torch.inference_mode():
         predict = model(tokenized_text)
 
-        vector = predict[0][:, 0, :].squeeze().cpu().numpy()
+        vector = np.array([predict[0][:, 0, :].squeeze().cpu().numpy()])
 
-    vector = np.array([vector])
     value_metrics, index = faiss_index.search(vector, k=top_k)
 
-    recommend_books = data_frame.iloc[index.reshape(top_k,)][['category_name', 'author', 'title', 'age', 'annotation']].reset_index(drop=True)
-    recommend_books = recommend_books.rename({'category_name': 'Жанр', 'author': 'Автор', 'title': 'Название книги', \
-                                              'age': 'Возрастное ограничение', 'annotation': 'Аннотация'}, axis=1)
+    value_metrics = np.round(value_metrics.reshape(top_k, ))
+    recommend_books = data_frame.iloc[index.reshape(top_k, ), 1:].reset_index(drop=True)
 
-    return recommend_books
+    return recommend_books, value_metrics
 
 
 
